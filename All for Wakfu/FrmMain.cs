e9c8d4796dec31a6679b98ac4ef160dab404c95d
dario-cfpt <34121538+dario-cfpt@users.noreply.github.com>
2018-03-08 16:42:01 +0100

    Création du projet & création de la base de données SQLite
    - Structure de la base de données :white_check_mark:
    
    Il reste à faire :
    - Remplissage des données pour les tables Rarety & Type_Items

--- All for Wakfu/FrmMain.cs
+++ All for Wakfu/FrmMain.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace All_for_Wakfu
 {
-    public partial class Form1 : Form
+    public partial class FrmMain : Form
     {
-        public Form1()
+        public FrmMain()
         {
             InitializeComponent();
         }
--- All for Wakfu/Program.cs
+++ All for Wakfu/Program.cs
@@ -16,7 +16,7 @@ namespace All_for_Wakfu
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmMain());
         }
     }
 }
